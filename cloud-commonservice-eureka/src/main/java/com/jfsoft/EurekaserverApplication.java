4bade42d6f9f99f6e11f7a3ea7acc9eaed423e8e
wanggang0302 <wanggangjava@126.com>
2017-06-28 22:45:27 +0800

    注册服务，PEIS服务提供者

---
+++ cloud-commonservice-eureka/src/main/java/com/jfsoft/EurekaserverApplication.java
@@ -0,0 +1,18 @@
+package com.jfsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by root on 2017/6/28 0028.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaserverApplication.class, args);
+    }
+
+}
